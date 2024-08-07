2a8c56d39ccedce2405eaa972ae3a92784fc4fcf
Arthur Barbosa <arthur.com50@gmail.com>
2024-08-07 20:50:08 -0300

    alteracoes no form login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,72 +1,116 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import api from "../api";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import { Input, Button } from "@nextui-org/react";
+import { EyeFilledIcon } from "../assets/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "../assets/EyeSlashFilledIcon";
+import doneAll from "../assets/done_all_black-removebg.png";
 
-function LoginForm({route, method}) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+function LoginForm({ route, method }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(""); // Estado para a mensagem de erro
+  const [isVisible, setIsVisible] = useState(false);
+  const navigate = useNavigate();
 
-    const title = method === "login" ? "Login" : "Register";
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
-    const handleSubmit = async (e) => {
-        setLoading(true);
-        e.preventDefault();
+  const title = method === "login" ? "Login" : "Register";
 
-        try { 
-            const res = await api.post(route, { username, password })
-            if(method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/months")
-            } else {
-                navigate("/login")
-            }
-        }
-        catch (error) {
-            alert(error)
-        }
-        finally {
-            setLoading(false)
-        }
-    };
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
 
-    return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+    try {
+      const res = await api.post(route, { username, password });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/months");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      setErrorMessage("Invalid username or password. Please try again."); // Define a mensagem de erro
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">{title}</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <h1 className="text-5xl font-bold">{title}</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleSubmit} className="card-body">
-                <div className="form-control">
-                    <label className="label">
-                    <span className="label-text">Username</span>
-                    </label>
-                    <input value={username} onChange={(e) => setUsername(e.target.value)} 
-                    type="text" placeholder="username" className="input input-bordered" required />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                    <span className="label-text">Password</span>
-                    </label>
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} 
-                    type="password" placeholder="password" className="input input-bordered" required />
-                    <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                    </label>
-                </div>
-                <div className="form-control mt-6">
-                    <button className="btn btn-primary" type="submit">{title}</button>
-                </div>
-            </form>
+          <form onSubmit={handleSubmit} className="card-body">
+            <div className="form-control">
+              <Input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                isRequired
+                label="User"
+                variant="bordered"
+                type="text"
+                labelPlacement="outside"
+                placeholder="Enter your User"
+                size="lg"
+              />
+            </div>
+            <div className="form-control">
+              <Input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                isRequired
+                variant="bordered"
+                label="Password"
+                labelPlacement="outside"
+                placeholder="Enter your Password"
+                size="lg"
+                endContent={
+                  <button
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={toggleVisibility}
+                    aria-label="toggle password visibility"
+                  >
+                    {isVisible ? (
+                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
+              />
+              {errorMessage && (
+                <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <Button
+                color="primary"
+                className="mt-8 h-12 text-white"
+                type="submit"
+                isLoading={loading}
+              >
+                <img src={doneAll} alt="Logo" className="h-5 w-5" />
+                {title} into Journaling
+              </Button>
+            </div>
+          </form>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
 
-export default LoginForm
+export default LoginForm;
